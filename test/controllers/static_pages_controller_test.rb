4e8645eddd48f9720a09911d12193853ebea6a59
danghanh <ldhanh1999@gmail.com>
2020-02-27 20:21:18 +0700

    done basic setup & layout

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Apartment"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Apartment"
+  end
+
+end
