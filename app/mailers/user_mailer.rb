b697aa63e6fdef48cc672280a5810a090393e192
danghanh <ldhanh1999@gmail.com>
2020-03-03 15:58:16 +0700

    merge develop

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,12 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
 
+<<<<<<< HEAD
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password reset"
   end
+=======
+  
+>>>>>>> develop
 end
