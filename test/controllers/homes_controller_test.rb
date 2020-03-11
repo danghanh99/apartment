ea5fe20acdbd8d1e29b653edb2504ebda78e3203
danghanh <ldhanh1999@gmail.com>
2020-03-11 14:46:11 +0700

    add test home controller

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -11,4 +11,21 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Home.count' do
+      delete home_path(@home)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    home = homes(:one)
+    assert_no_difference 'Home.count' do
+      delete home_path(home)
+    end
+    assert_redirected_to root_url
+  end
+
 end
