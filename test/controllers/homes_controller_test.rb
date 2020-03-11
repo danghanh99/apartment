68c8e77c7154857a388ee465df3f334b2eac7478
danghanh <ldhanh1999@gmail.com>
2020-03-11 14:08:17 +0700

    add test controller home

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class HomesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @home = homes(:one)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Home.count' do
+      post homes_path, params: { home: { name: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
 end
