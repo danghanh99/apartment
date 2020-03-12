afac0478ee37426c890a6746c83a5c53b4586286
danghanh <ldhanh1999@gmail.com>
2020-03-12 21:52:57 +0700

    use present

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,17 @@
 module SessionsHelper
 
-    # Logs in the given user.
-    def log_in(user)
-      session[:user_id] = user.id
-    end
-  
-    # Remembers a user in a persistent session.
-    def remember(user)
-      user.remember
-      cookies.permanent.signed[:user_id] = user.id
-      cookies.permanent[:remember_token] = user.remember_token
-    end
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-    
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
@@ -26,26 +25,26 @@ module SessionsHelper
     end
   end
 
-    # Returns true if the user is logged in, false otherwise.
-    def logged_in?
-      !current_user.nil?
-    end
-    
-    def admin?
-      current_user.present? && current_user.admin?
-    end
-    # Forgets a persistent session.
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    current_user.present?
+  end
+
+  def admin?
+    current_user.present? && current_user.admin?
+  end
+
+  # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
 
-    # Logs out the current user.
+  # Logs out the current user.
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
-  
-  end
+end
