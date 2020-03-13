efe88ecc0d05f0a5b0f3a15a99b82f7de7084017
danghanh <ldhanh1999@gmail.com>
2020-03-13 15:12:37 +0700

    replace update_attribute to update

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,7 @@ class User < ApplicationRecord
 
   # Activates an account.
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update(activated: true, activated_at: Time.zone.now)
   end
 
   # Sends activation email.
@@ -55,8 +54,7 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
