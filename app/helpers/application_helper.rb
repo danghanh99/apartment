e1de78193154542382eae0b6b6196535981f5174
danghanh <ldhanh1999@gmail.com>
2020-03-19 17:19:39 +0700

    add def format currency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,22 @@
 module ApplicationHelper
 
-    # Returns the full title on a per-page basis.
-    def full_title(page_title = '')
-      base_title = "Apartment"
-      if page_title.empty?
-        base_title
-      else
-        page_title + " | " + base_title
-      end
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = "")
+    base_title = "Apartment"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
     end
   end
+
+  def format_currency(price, price_unit)
+    price_symbol = "VND"
+    if price_unit == "USD"
+      price_symbol = "$"
+      " #{price_symbol}#{price}"
+    else
+      " #{price}#{price_symbol}"
+    end
+  end
+end
